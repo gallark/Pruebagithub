07a8dfd7c82200cd9830386463494b892eec104b
erickmax3 <erickmax3@gmail.com>
2019-11-19 05:52:43 -0500

    Iniciando el proyecto. Vers. 1.0

---
+++ springboot-test-git/src/main/java/com/pmc/springboot/app/config/SpringbootTestGitApplication.java
@@ -0,0 +1,13 @@
+package com.pmc.springboot.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestGitApplication.class, args);
+	}
+
+}
